e82e97c5bfe07e541171b7e3c6bda4538191f521
MathumathiV <38651071+MathumathiV@users.noreply.github.com>
2018-04-24 12:25:26 +0530

    Update +,-,0.c

--- Beginner Level/+,-,0.c
+++ Beginner Level/+,-,0.c
@@ -2,23 +2,17 @@
 void main()
 {
   int n;
-  printf("Enter a number:");
   scanf("%d",&n);
-  if(n>=1&&n<=100000)
-  {
+
   if(n<0)
-  {printf("YOUR INPUT IS NEGATIVE");
+  {printf("Negative");
   }
-  else if(n=0)
+  else if(n==0)
   {
-    printf("YOUR INPUT IS ZERO");
+    printf("Zero");
   }
   else
   {
-    printf("YOUR INPUT IS POSITIVE");
-  }}
-  else
-  {
-    printf("Enter the number between 1 to 100000");
+   printf("Positive");
   }
 }
